f1a6f8d021b23a0fd97d072e147c8c9a18f66ca2
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2018-11-07 03:39:14 -0300

    🌐

---
+++ projeto-pronto/React/build/precache-manifest.a49dc5d1e8237e7e2787341e5cfc4ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44cf2d161f4ffcd65a5a",
+    "url": "/static/js/main.44cf2d16.chunk.js"
+  },
+  {
+    "revision": "d817bf5b9057619f9ee6",
+    "url": "/static/js/1.d817bf5b.chunk.js"
+  },
+  {
+    "revision": "44cf2d161f4ffcd65a5a",
+    "url": "/static/css/main.2660d025.chunk.css"
+  },
+  {
+    "revision": "a7c2312a2375abdb0571ae27730e9c45",
+    "url": "/index.html"
+  }
+];
