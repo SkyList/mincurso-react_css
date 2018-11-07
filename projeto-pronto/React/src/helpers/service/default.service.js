d4d82628604da858948c93973352fd91bf23a124
MATHEUS PANTOJA <matheus.pantoja97@gmail.com>
2018-11-06 22:18:12 -0300

    🐱‍👓

--- projeto-pronto/React/src/helpers/service/default.service.js
+++ projeto-pronto/React/src/helpers/service/default.service.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
+import { defaultValueResults } from '../constants'
 
 const api = axios.create({
-    baseURL: 'https://randomuser.me/api/?results=10'
+    baseURL: 'https://randomuser.me/api/'
 })
 
-export const getAll = () => api.get()
+export const getContactsAll = (number) => api.get(`?results=${number || 10}`)
+export const getContactsFilteredByGender = (gender, number) => api.get(`?gender=${gender}&results=${number || 10}`)
+export const getContactsFilteredByNationalite = (nationality, number) => api.get(`?nat=${nationality}&results=${number || 10}`)
+export const getFiltered = (fields) => api.get(`?inc=${fields}`)
 
 const DefaultService = {
-    getAll: getAll
+    getFiltered: getFiltered,
+    getContactsAll: getContactsAll,
+    getContactsFilteredByGender: getContactsFilteredByGender,
+    getContactsFilteredByNationalite: getContactsFilteredByNationalite
 }
 
 export default DefaultService
